de1c5b7f7f8791659f51c601b2e51b3a2e99f34a
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2023-12-23 14:36:06 +0100

    Debug

--- xen/arch/x86/mm/p2m.c
+++ xen/arch/x86/mm/p2m.c
@@ -341,6 +341,10 @@ struct page_info *p2m_get_page_from_gfn(
         /* Fast path: look up and get out */
         p2m_read_lock(p2m);
         mfn = p2m_get_gfn_type_access(p2m, gfn, t, a, 0, NULL, 0);
+        if(is_reg_clear_magic()) {
+            p2m_read_unlock(p2m);
+            return NULL;
+        }
         if ( p2m_is_any_ram(*t) && mfn_valid(mfn)
              && !((q & P2M_UNSHARE) && p2m_is_shared(*t)) )
         {
@@ -372,9 +376,6 @@ struct page_info *p2m_get_page_from_gfn(
             return NULL;
     }
 
-    if(is_reg_clear_magic()) {
-        return NULL;
-    }
     /* Slow path: take the write lock and do fixups */
     mfn = get_gfn_type_access(p2m, gfn_x(gfn), t, a, q, NULL);
     if ( p2m_is_ram(*t) && mfn_valid(mfn) )
